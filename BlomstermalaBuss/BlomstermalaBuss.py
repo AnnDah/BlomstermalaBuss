ec8a572903ea182dd532b959e63926033436a29d
Annika Magnusson <annika.h.magnusson@gmail.com>
2014-10-14 15:17:41 +0200

    Test functions

--- BlomstermalaBuss/BlomstermalaBuss.py
+++ BlomstermalaBuss/BlomstermalaBuss.py
@@ -1,7 +1,31 @@
 import QuerySQL
+import dbConnector
 
 class BlomstermalaBuss(object):
-    query = QuerySQL.QuerySQL()
-    print query.get_all_users()
+    print 'Vad vill du göra?'
+    print '1: Se alla personer'
+    print '2: Lägga till person'
+    print '3: Ta bort person'
+    choice = raw_input('Val: ')
+
+    if choice == '1':
+        #print all persons
+        print QuerySQL.QuerySQL().get_all_users()
+
+    elif choice == '2':
+        #add person
+        first_name = raw_input('Add firstname: ')
+        last_name = raw_input('Add last name: ')
+        personal_number = raw_input('Add personal number: ')
+        QuerySQL.QuerySQL().add_user(first_name, last_name, personal_number)
+
+    elif choice == '3':
+        #delete person
+        id = raw_input('Add ID to person to delete: ')
+        QuerySQL.QuerySQL().delete_user(id)
+
+    else:
+        print 'Det var inget giltligt val :)'
+
     
 
--- BlomstermalaBuss/Person.py
+++ BlomstermalaBuss/Person.py
@@ -1,4 +1,10 @@
+import QuerySQL
+
 class Person(object):
     """description of class"""
+    query = QuerySQL.QuerySQL()
+    def add_user(FirstName, LastName, PersonalNumber):
+        
+        print 'Person added'
 
 
