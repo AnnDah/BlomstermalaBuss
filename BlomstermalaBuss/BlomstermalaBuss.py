1221b62f9b699846c68374aa6b98452db373e426
Annika Magnusson <annika.h.magnusson@gmail.com>
2014-10-15 12:40:27 +0200

    Added manu

--- BlomstermalaBuss/BlomstermalaBuss.py
+++ BlomstermalaBuss/BlomstermalaBuss.py
@@ -1,40 +1,73 @@
-import QuerySQL #ta bort
-import dbConnector #ta bort
 import main
 
 class BlomstermalaBuss(object):
-    print 'Vad vill du göra?'
-    print '1: Se alla personer'
-    print '2: Lägga till person'
-    print '3: Ta bort person'
-    choice = raw_input('Val: ')
+    
+    print 'Are you a:'
+    print '1: User'
+    print '2: Admin'
+    choice = raw_input('Enter choice: ')
 
     if choice == '1':
-        #print all persons
-        print QuerySQL.QuerySQL().get_all_users()
+        print 'user menu'
 
     elif choice == '2':
-        #add person
-        first_name = raw_input('Add firstname: ')
-        last_name = raw_input('Add last name: ')
-        personal_number = raw_input('Add personal number: ')
-        main.main().add_user(first_name, last_name, personal_number)
+        print 'Enter menu to go to'
+        print '1: Person'
+        print '2: Bus'
+        print '3: Trip'
+        print '4: Booking'
+        choice = raw_input('Enter choice: ')
 
-    elif choice == '3':
-        #delete person
-        id = raw_input('Add ID to person to delete: ')
-        main.main().delete_person(id)
+        if choice == '1':
+            print 'Vad vill du göra?'
+            print '1: Se alla personer'
+            print '2: Lägga till person'
+            print '3: Ta bort person'
+            choice = raw_input('Val: ')
 
-    elif choice == '4':
-        #Add bus                 
-        name = raw_input('Add Name: ')
-        seats = raw_input('Add Seats ')
-        main.main().add_bus(name, seats)
+            if choice == '1':
+                #print all persons
+                print QuerySQL.QuerySQL().get_all_users()
+
+            elif choice == '2':
+                #add person
+                first_name = raw_input('Add firstname: ')
+                last_name = raw_input('Add last name: ')
+                personal_number = raw_input('Add personal number: ')
+                main.main().add_user(first_name, last_name, personal_number)
+
+            elif choice == '3':
+                #delete person
+                id = raw_input('Add ID to person to delete: ')
+                main.main().delete_person(id)
         
+            else:
+                print 'Det var inget giltligt val :)'
 
+        elif choice == '2':
+            print 'What do you want to do?'
+            print '1: Add bus'
+            print '2: Edit bus'
+            print '3: Delete bus'
+            choice = raw_input('Enter choice: ')
 
-    else:
-        print 'Det var inget giltligt val :)'
+            if choice == '1':
+                #Add bus                 
+                name = raw_input('Add Name: ')
+                seats = raw_input('Add Seats ')
+                main.main().add_bus(name, seats)
 
-    
+            elif choice == '2':
+                print 'edit bus'
 
+            elif choice == '3':
+                print 'delete bus'
+
+            else:
+                print 'Invalid choice!'
+
+        elif choice =='3':
+            print 'trip menu' 
+
+        elif choice == '4':
+            print 'booking menu'
