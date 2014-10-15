337074b7e84df78f988938f4a06fcf1163e17588
Annika Magnusson <annika.h.magnusson@gmail.com>
2014-10-15 10:49:36 +0200

    Added comments in main

--- BlomstermalaBuss/main.py
+++ BlomstermalaBuss/main.py
@@ -1,4 +1,24 @@
 class main(object):
     """description of class"""
 
+    #add person
+    #edit person
+    #delete person
+    
+    #add bus
+    #edit bus
+    #delete bus
+
+    #add city
+    #edit city
+    #delete city
+
+    #add trip
+    #edit trip
+    #delete trip
+
+    #add booking
+    #change booking
+    #delete booking
+
 
